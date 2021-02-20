bd12c3bef0264a6a77fc34b76d1bc2b7de644c72
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-02-19 19:01:07 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TodoList = () => {
+    const [posts, setPosts] = useState();
 
-export default App;
+    useEffect (() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((res0) => {
+            const setPosts = res.data;
+            setPosts(setPosts);
+        });
+    }, []);
+    return (
+        <div>
+           <h1>To Do List</h1>
+            {posts &&
+            posts.map((todo) => {
+            const {id, title} = posts;
+            return (
+              <div key={id}>
+                <h2>{title}</h2>
+                <h3>Information: {id}</h3>
+              </div>
+                )
+            })}
+        </div>
+    );
+  };
