af4661469f13aec2153f741e324d5e298d7a5b47
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-02-19 19:10:31 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Example from './components/example/index';
 
-const TodoList = () => {
-    const [posts, setPosts] = useState();
+function App() {
+  return (
+    <Example/>
 
-    useEffect (() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts').then((res0) => {
-            const setPosts = res.data;
-            setPosts(setPosts);
-        });
-    }, []);
-    return (
-        <div>
-           <h1>To Do List</h1>
-            {posts &&
-            posts.map((todo) => {
-            const {id, title} = posts;
-            return (
-              <div key={id}>
-                <h2>{title}</h2>
-                <h3>Information: {id}</h3>
-              </div>
-                )
-            })}
-        </div>
-    );
-  };
+  );
+}
+
+export default App;
---
+++ src/components/Example/index.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+class Example extends React.Component {
+    render() {
+        return <p>My name is Kensley</p>;
+    }
+}
+
+export default Example;
